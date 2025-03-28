f1e73f97e0196dcdcafec4a7a5bff360e457900c
Cotrim12 <cotrimdasilvafelippe908@gmail.com>
2025-03-28 11:53:09 -0300

    calculadora

---
+++ tipos_de_dados/operadores.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(int argc, char* argv []){
+    
+    float n1, n2;
+
+    printf("Digite o valor de n1: ");
+    scanf("%f", &n1);
+
+    printf("Digite o valor de n2: ");
+    scanf("%f", &n2);    
+
+    // Adição
+    float soma = n1 + n2;
+    printf ("%f + %f = %f", n1, n2, soma);
+
+    // Subtração
+
+
+    // Multiplicação
+
+    // Potenciação
+
+    // Radiciação
+
+    // Resto da divisão
+
+    // Quociente da divisão
+
+    // Incremento
+
+    //Decremento
+
+    return 0;
+}
