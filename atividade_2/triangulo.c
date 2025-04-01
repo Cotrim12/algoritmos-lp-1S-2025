0c58fcc1990b94b5b3fcd6b6d03cbfe6e7088486
Cotrim12 <cotrimdasilvafelippe908@gmail.com>
2025-04-01 18:36:40 -0300

    triangulo

---
+++ atividade_2/triangulo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Função para calcular a área do triângulo
+float calcular_area_triangulo(float base , float altura){
+    return (base * altura) / 2;
+
+}
+
+int main (){
+    float base, altura;
+
+    // Solicita os valores da base e da altura do triângulo
+    printf("Digite o valor da base do triangulo: ");
+    scanf("%f", &base);
+    printf("Digite o valor da altura do triangulo: ");
+    scanf("%f", &altura);
+
+    // Calcula a área
+    float area = calcular_area_triangulo(base, altura);
+
+    // Exibe o resultado
+    printf("A area do triangulo eh: %.2f\n", area);
+
+    return 0;
+}
