66d32c85a18b1d4d4c0906d8209e6a7b3a105c74
Cotrim12 <cotrimdasilvafelippe908@gmail.com>
2025-04-04 12:57:00 -0300

    filmes atividade

--- atividade_2/area.c
+++ atividade_2/area.c
@@ -19,11 +19,11 @@ char opcao;
 
     scanf("%c", &opcao);
     
-    switch (opcao){
+    switch (opcao)
     
 case 'c':
     printf("Circulo\n");
-    float calcular_area_circulo(float raio) {
+    float calcular_area_circulo(float raio) 
 return PI * raio * raio;
 printf("Digite o valor do raio do circulo: ");
     scanf("%f", &raio);
@@ -38,18 +38,63 @@ printf("Digite o valor do raio do circulo: ");
     break;
 case 'l':
     printf("Losango\n");
+    float calcular_area_losango (float diagonal_maior, float diagonal_menor) 
+    return (diagonal_maior * diagonal_menor) / 2;
+     printf("Digite o valor da diagonal maior: ");
+    scanf("%f", &diagonal_maior);
+    printf("Digite o valor da diagonal menor: ");
+    scanf("%f", &diagonal_menor);
+     float area = calcular_area_losango(diagonal_maior, diagonal_menor);
+     printf("A area do losango eh: %.2f\n", area);
+
     break;
 case'q':
     printf("Quadrado\n");
+    float calcular_area_quadrado(float lado) 
+    return lado * lado;
+    printf("Digite o valor do lado do quadrado:");
+    scanf("%f", &lado);
+     float area = calcular_area_quadrado(lado);
+     printf("A area do quadrado eh: %2f\n", area);
+
     break;
 case'r':
     printf("Retangulo\n");
+    float calcular_area_retangulo(float base, float altura)
+    return base * altura;
+    printf("Digite o valor da base do retangulo:");
+    scanf("%f", &base);
+    printf("Digite o valor da altura do retangulo:");
+    scanf("%f",  &altura);
+    float area = calcular_area_retangulo(base, altura);
+    printf(" A area do retangulo eh: %.2f\n", area);
+
     break;
 case't':
     printf("Triangulo\n");
+    float calcular_area_triangulo(float base , float altura){
+    return (base * altura) / 2;
+    printf("Digite o valor da base do triangulo: ");
+    scanf("%f", &base);
+    printf("Digite o valor da altura do triangulo: ");
+    scanf("%f", &altura);
+     float area = calcular_area_triangulo(base, altura);
+      printf("A area do triangulo eh: %.2f\n", area);
+
     break;
 case'z':
     printf("Trapezio\n");
+    float calcular_area_trapezio(float base_maior, float base_menor, float altura) {
+    return ((base_maior + base_menor) * altura) / 2;
+     printf("Digite o valor da base maior: ");
+    scanf("%f", &base_maior);
+    printf("Digite o valor da base_menor: ");
+    scanf("%f", &base_menor);
+    printf("Digite o valor da altura do trapezio: ");
+    scanf("%f", &altura);
+     float area = calcular_area_trapezio(base_maior, base_menor, altura);
+      printf("A area do trapezio eh : %.2f\n", area);
+
     break;
 default:
     printf("Opcao invalida");
