fa470db9a55d736ebfe1c91ade68767cf9f91480
Cotrim12 <cotrimdasilvafelippe908@gmail.com>
2025-04-11 11:11:27 -0300

    aula 11/04

---
+++ 1_100/numero1_100.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (int agrc, char* argv[]) {
+
+
+    int limite, i;
+    limite = 100;
+    for ( i = 0; i <= limite; i++)
+    printf("Numero %d\n", i);
+    
+   return 0; 
+    
+}
